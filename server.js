3edf7b62028f56d75e3b951db6aa62b6658fd47c
Mohamed Infaz <infaz9999@gmail.com>
2018-03-25 16:57:49 +0530

    My Initial Commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('unable to log data to file')
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) =>{
+    res.render('maintenance.hbs', {
+        maintenanceMessage : 'We will be back shortly'
+    });
+});
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+/*
+app.get('/', (req, res) => {
+    
+    res.send('<h1>Hello Express</h1>');
+    res.send({
+       name : 'Infaz',
+        likes : [
+            'sleeping', 'Gaming', 'Watching Cricket'
+        ]
+    });
+});*/
+
+app.get('/', (req, res) => {
+   
+    res.render('home.hbs',{
+        pageTitle : 'Home Page',
+        welcomeMessage : 'Welcome to the Page'
+    });
+});
+
+app.get('/about',(req,res) => {
+    //res.send('This is about page');
+    res.render('about.hbs', {
+        pageTitle : 'Webservers About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage : 'Unable to load data'
+    });
+});
+
+app.listen('3000');
